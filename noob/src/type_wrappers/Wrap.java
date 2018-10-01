529bf7ef5534e31d854fc7f431d519dcee9e7c6d
Vipul Sharma <vipuls526@gmail.com>
2018-10-01 10:53:15 +0530

    type wrappers

---
+++ noob/src/type_wrappers/Wrap.java
@@ -0,0 +1,20 @@
+package type_wrappers;
+
+/*
+ * Integer
+ * Float
+ * Double
+ * Long
+ * Short
+ * Byte
+ * Character
+ * Boolean
+ * */
+
+public class Wrap {
+	public static void main(String args[]) {
+		Integer intob = new Integer(100);
+		int i = intob.intValue();
+		System.out.println(i);
+	}
+}

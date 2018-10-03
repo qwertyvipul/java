7ef1f67af931176261bf32793c3012b2dee5fc2f
Vipul Sharma <vipuls526@gmail.com>
2018-10-04 00:06:08 +0530

    javafx

---
+++ javafx/src/oracle/HelloWorld.java
@@ -0,0 +1,32 @@
+package oracle;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class HelloWorld extends Application{
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("Hello World!");
+		Button btn = new Button();
+		btn.setText("Hello World!");
+		btn.setOnAction(new EventHandler<ActionEvent>(){
+			public void handle(ActionEvent event) {
+				System.out.println("Hello World!");
+			}
+		});
+		
+		StackPane root = new StackPane();
+		root.getChildren().add(btn);
+		primaryStage.setScene(new Scene(root, 400, 300));
+		primaryStage.show();
+	}
+}
---
+++ javafx/src/reference/FxButtonEvent.java
@@ -0,0 +1,48 @@
+package reference;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+public class FxButtonEvent extends Application{
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("Button Event");
+		FlowPane rootNode = new FlowPane(10, 10);
+		
+		//Position the controls in center
+		rootNode.setAlignment(Pos.CENTER);
+		
+		Scene mainScene = new Scene(rootNode, 300, 100);
+		primaryStage.setScene(mainScene);
+		
+		Label response = new Label("Push a Button!");
+		Button btnAlpha = new Button("Alpha");
+		Button btnBeta = new Button("Beta");
+		
+		btnAlpha.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent e) {
+				response.setText("Alpha Button Clicked!");
+			}
+		});
+		
+		btnBeta.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent e) {
+				response.setText("Beta Button Clicked!");
+			}
+		});
+		
+		rootNode.getChildren().addAll(btnAlpha, btnBeta, response);
+		primaryStage.show();
+	}
+}

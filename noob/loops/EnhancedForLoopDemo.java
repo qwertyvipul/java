ecfa7d7b5f6180c05eaca1ad4e1d14b9c4fd9a18
Vipul Sharma <vipuls526@gmail.com>
2019-02-24 15:25:56 +0530

    Create EnhancedForLoopDemo.java

---
+++ noob/loops/EnhancedForLoopDemo.java
@@ -0,0 +1,8 @@
+class EnhancedForLoopDemo{
+    public static void main(String args[]){
+        int array[] = {1, 2, 3, 4, 5};
+        for (int val : array){
+            System.out.println(val);
+        }
+    }
+}
